9ec0422f1daa6e0bac02055246aba01bacf42884
João Palma <joao.palma@nearseatech.io>
2023-05-19 11:33:07 +0100

    aula 5

--- nodejs 5 - project struct/routes/todo.js
+++ nodejs 5 - project struct/routes/todo.js
@@ -1,9 +1,20 @@
 import Router from 'express';
-import { getAllTodos } from '../controllers/todo.controller.js';
+import {
+  getAllTodos,
+  getById,
+  create,
+  update,
+  deleteTodo,
+} from '../controllers/todo.controller.js';
 
 //--ROUTES--//
 const todoRoutes = Router();
 
+// http://localhost:4242/api/todo/getAll
 todoRoutes.get('/getAll', getAllTodos);
+todoRoutes.get('/getById/:idGet', getById);
+todoRoutes.post('/create', create);
+todoRoutes.put('/update/:idUpdate', update);
+todoRoutes.delete('/delete/:idDelete', deleteTodo);
 
 export { todoRoutes };
